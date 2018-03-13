0bed63196a072323b1ce86c5f00fd31c56c5717f
Connor Weeks <connorweeks1@gmail.com>
2018-03-13 10:48:12 -0600

    Added arduino code for Mega, NodeMCU, and started the Node.js server

---
+++ TicketToRide/constants.h
@@ -0,0 +1,17 @@
+enum players {
+  RED,
+  YELLOW,
+  GREEN,
+  BLUE,
+  BLACK,
+  NUMBER_OF_PLAYERS
+};
+
+// These values were produced with Resistor Calculations.ods in the repo
+const int ONE_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {839, 697, 563, 410, 238};
+const int TWO_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {823, 673, 536, 384, 219};
+const int THREE_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {823, 673, 536, 384, 219};
+const int FOUR_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {808, 652, 514, 363, 205};
+const int FIVE_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {799, 639, 499, 350, 196};
+const int SIX_TRAIN_VOLTAGES[NUMBER_OF_PLAYERS] = {799, 640, 500, 351, 196};
+
